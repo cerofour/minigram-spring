84a8b975a7abf217d7bd8237c4e289ad7d11fc82
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2025-12-17 00:43:51 -0500

    idk how SES works so lets keep it this way

--- src/main/java/com/cerofour/MiniGram/notification/infrastructure/ExternalNotificationAdapter.java
+++ src/main/java/com/cerofour/MiniGram/notification/infrastructure/ExternalNotificationAdapter.java
@@ -2,9 +2,15 @@ package com.cerofour.MiniGram.notification.infrastructure;
 
 import com.cerofour.MiniGram.notification.application.NotificationPort;
 import com.cerofour.MiniGram.shared.domain.UseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 @UseCase
+@RequiredArgsConstructor
 public class ExternalNotificationAdapter implements NotificationPort {
+
+    private final MailSender mailSender;
 
     @Override
     public void suscribeToEmailService(String email) {
@@ -13,6 +19,11 @@ public class ExternalNotificationAdapter implements NotificationPort {
 
     @Override
     public void sendNotification(String email, String message) {
-        System.out.printf("Sending message: '%s' to '%s'", message, email);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("llacsahuanga.buques@gmail.com");
+        simpleMailMessage.setTo(email);
+        simpleMailMessage.setSubject("test subject");
+        simpleMailMessage.setText(message);
+        this.mailSender.send(simpleMailMessage);
     }
 }
